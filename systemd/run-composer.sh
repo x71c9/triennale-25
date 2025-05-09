c72900b2a458c054756760b58c2b6505527f9bde
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-05-09 22:43:57 +0200

    feat: full path nix-shell

--- systemd/run-composer.sh
+++ systemd/run-composer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/dafne/triennale-25 || exit 1
-exec nix-shell shell.nix --run "cd composer && cargo run installation start"
+exec /nix/var/nix/profiles/default/bin/nix-shell shell.nix --run "cd composer && cargo run installation start"
 
