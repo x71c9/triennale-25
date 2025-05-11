f928cd05ba7a261eae4dece94d680ef1ca6f723b
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-05-11 12:46:48 +0200

    feat: update abs path shell.nix

--- systemd/run-director.sh
+++ systemd/run-director.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/dafne/triennale-25 || exit 1
-exec /nix/var/nix/profiles/default/bin/nix-shell shell.nix --run "cd director && bash start.sh"
+exec /nix/var/nix/profiles/default/bin/nix-shell /home/dafne/triennale-25/shell.nix --run "cd director && bash start.sh"
 
