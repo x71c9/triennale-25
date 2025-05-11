d9ace2c8303325708855846b468909322f9b473d
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-05-11 17:40:26 +0200

    fix: sprinklers

--- director/start.sh
+++ director/start.sh
@@ -24,10 +24,10 @@ while true; do
 
   if [ $((RANDOM % 10)) -eq 0 ]; then
     robot_id=$(( (RANDOM % 3) + 1 ))
-    scmd="cargo run sparklings $sparkling_id on --no-dry-run"
+    scmd="cargo run s $sprikler_id on --no-dry-run"
     echo -e "\n[$(date +'%H:%M:%S')] Running: $scmd"
     sleep 20
-    scmd="cargo run sparklings $sparkling_id off --no-dry-run"
+    scmd="cargo run s $sprikler_id off --no-dry-run"
     echo -e "\n[$(date +'%H:%M:%S')] Running: $scmd"
   fi
 
