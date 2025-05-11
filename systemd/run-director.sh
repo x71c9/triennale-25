d7442c0bb8861a88576d09779ac43bc127328fa9
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-05-11 12:12:55 +0200

    feat: system with start.sh

--- systemd/run-director.sh
+++ systemd/run-director.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/dafne/triennale-25 || exit 1
-exec /nix/var/nix/profiles/default/bin/nix-shell shell.nix --run "cd director && cargo run installation start"
+exec /nix/var/nix/profiles/default/bin/nix-shell shell.nix --run "cd director && bash start.sh"
 
